2faa1eaffeeb80fa5d1949d6adc1fbd6f46c6a7a
Prabhash Singh <prabhash>
2021-08-23 17:25:29 +0530

    [typo]

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import friendContact  from './components/friendContact.vue';
+import friendContact  from './friendContact.vue';
 
 const app = createApp(App).mount('#app');
 
